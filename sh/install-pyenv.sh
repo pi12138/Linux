d394a5a8909f9ef9b55334f0ab421302d0908188
zyp <zyp@nb.com>
2021-07-19 09:24:03 +0800

    shell

---
+++ sh/install-pyenv.sh
@@ -0,0 +1,9 @@
+git clone https://github.com.cnpmjs.org/pyenv/pyenv.git ~/.pyenv && echo '下载 pyenv 成功'
+git clone https://github.com.cnpmjs.org/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+echo '\n# pyenv settings' >> ~/.zshrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+echo '配置 pyenv 完成'
