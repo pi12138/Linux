ab778f8928c093f37a38762c9542583885868cf3
zyp <zhou.youpeng@foxmail.com>
2021-09-19 21:51:52 +0800

    new shell

---
+++ sh/install-python-depend.sh
@@ -0,0 +1,5 @@
+# 安装 python 依赖
+
+sudo apt install gcc
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+
