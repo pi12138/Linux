ac868fd0f0e52ec6dbdd8c8227e913e09f728cf7
zyp <zhou.youpeng@foxmail.com>
2022-03-13 21:50:59 +0800

    vim config

---
+++ config/.vimrc
@@ -0,0 +1,11 @@
+set nu
+set number
+
+"set cursorcolumn
+
+set showmatch
+set tabstop=4
+set shiftwidth=4
+set autoindent
+set paste
+
--- sh/install-oh-my-zsh.sh
+++ sh/install-oh-my-zsh.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 set -e
 
+if [ $OHMYZSH_HOME ]; then
+	ohmyzsh_home=$OHMYZSH_HOME
+else
+	ohmyzsh_home="https://github.com/ohmyzsh/ohmyzsh.git"
+fi
+
+if [ $ZSH_AUTOSUGGESTIONS_HOME ]; then
+	zsh_autosuggestions_home=$ZSH_AUTOSUGGESTIONS_HOME
+else
+    zsh_autosuggestions_home="https://github.com/zsh-users/zsh-autosuggestions.git"
+fi
+
+if [ $ZSH_SYNTAX_HIGHLIGHTING_HOME ]; then
+    zsh_syntax_highlighting_home=$ZSH_SYNTAX_HIGHLIGHTING_HOME
+else
+    zsh_syntax_highlighting_home="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+fi
+
+echo $ohmyzsh_home
+echo $zsh_autosuggestions_home
+echo $zsh_syntax_highlighting_home
+
 echo '开始从 github 下载 oh-my-zsh'
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh && echo '下载 oh-my-zsh 完成'
+git clone $ohmyzsh_home ~/.oh-my-zsh && echo '下载 oh-my-zsh 完成'
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc && echo '创建 zshrc 成功'
 
 
 echo '开始为 oh-my-zsh 安装插件'
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && echo '下载 zsh-autosuggestions 完成'
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && echo '下载 zsh-syntax-highlighting 完成'
+git clone $zsh_autosuggestions_home ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && echo '下载 zsh-autosuggestions 完成'
+git clone $zsh_syntax_highlighting_home ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && echo '下载 zsh-syntax-highlighting 完成'
 echo 'ZSH_THEME="ys"' >> ~/.zshrc && echo '修改主题为 ys'
 echo 'plugins=(
     git
