7277c38d96be5c527033b0a6565c268bffb737f8
zyp <zhou.youpeng@foxmail.com>
2022-03-16 23:55:16 +0800

    feat: 更新安装 pyenv 的脚本

--- sh/install-pyenv.sh
+++ sh/install-pyenv.sh
@@ -1,9 +1,29 @@
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv && echo '下载 pyenv 成功'
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+#!/bin/bash
+set -e
+
+if [ $PYENV_HOME ]; then
+	pyenv_home=$PYENV_HOME
+else
+	pyenv_home="https://github.com/pyenv/pyenv.git"
+fi
+
+if [ $PYENV_VIRTUALENV_HOME ]; then
+	pyenv_virtualenv_home=$PYENV_VIRTUALENV_HOME
+else
+    pyenv_virtualenv_home="https://github.com/pyenv/pyenv-virtualenv.git"
+fi
+
+echo $pyenv_home
+echo $pyenv_virtualenv_home
+
+
+git clone $pyenv_home ~/.pyenv && echo '下载 pyenv 成功'
+git clone $pyenv_virtualenv_home ~/.pyenv/plugins/pyenv-virtualenv
 
 echo '\n# pyenv settings' >> ~/.zshrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
 echo '配置 pyenv 完成'
