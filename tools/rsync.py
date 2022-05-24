45bed0b61bd45ba07005bcd2cfde6fdbcf22eb1d
pipipipip <zhou.youpeng@foxmail.com>
2022-05-24 10:13:13 +0800

    feat: rsync tools

--- tools/rsync.py
+++ tools/rsync.py
@@ -11,14 +11,40 @@ import subprocess
 import sys
 
 
+RSYNC_FORMAT = 'rsync -avr --exclude={{{exclude}}} {source} {target}'
+
+
 class Config:
-    SOURCE_DIR = ''
-    TARGET_DIR = ''
+    SOURCE_DIR = './'  # 源目录
+    TARGET_DIR = ''  # 目标目录
+    EXCLUDE_RULES = ''  # 排除文件规则
+
+
+class Rsync:
+    def __init__(self, config, target_file):
+        self.config = config
+        self.target_file = target_file
+
+    def run(self):
+        source_file = f"{self.config.SOURCE_DIR.rstrip('/')}/{self.target_file}"
+        target_file = f"{self.config.TARGET_DIR.rstrip('/')}/{self.target_file}"
+
+        cmd = RSYNC_FORMAT.format(
+            source=source_file,
+            target=target_file,
+            exclude=self.config.EXCLUDE_RULES
+        )
+        print(f'rsync file: {self.target_file}')
+        status, output = subprocess.getstatusoutput(cmd)
+        if status != 0:
+            print(f'rsync error: {output}')
 
 
 def get_diff_file_list():
     cmd = 'git diff master --name-only'
     status, output = subprocess.getstatusoutput(cmd)
+    if status != 0:
+        print(f'git diff error: {output}')
     file_name_list = output.split('\n')
     return file_name_list
 
@@ -32,9 +58,18 @@ def get_config(config_file_path=None) -> Config:
 
     config = Config()
     for key, value in data.items():
-        setattr(config, key, value)
+        setattr(config, key.upper(), value)
     return config
 
 
 def main():
-    rsync_cmd_list = []
+    diff_file_list = get_diff_file_list()
+    config = get_config()
+
+    for file_name in diff_file_list:
+        rsync = Rsync(config, file_name)
+        rsync.run()
+
+
+if __name__ == '__main__':
+    main()
