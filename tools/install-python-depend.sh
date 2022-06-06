8a7a71fa76cdb645b57a05283614a8c56b2f2913
zyp <zhou.youpeng@foxmail.com>
2022-06-06 09:41:21 +0800

    add new line

--- tools/install-python-depend.sh
+++ tools/install-python-depend.sh
@@ -3,3 +3,4 @@
 sudo apt install gcc
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libpq-dev python-dev
 sudo apt install libssl-dev libcrypto++-dev -y
+sudo apt install lzma-dev -y
