daa4025df5d30e45fb737116a92e7ca07048a926
zyp <zyp>
2024-09-10 11:52:54 +0800

    feat: neovim snip build

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -5,7 +5,7 @@ return {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/nvim-cmp',
-    'L3MON4D3/LuaSnip',
+    {'L3MON4D3/LuaSnip', build = "make install_jsregexp"},
     'saadparwaiz1/cmp_luasnip',
 }
 
